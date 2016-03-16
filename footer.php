e50e3e34a2a1f11e8da492460927bb3babb188ad
Joe Snell <joesnellpdx@gmail.com>
2016-03-16 10:50:17 -0700

    base setup

--- footer.php
+++ footer.php
@@ -15,9 +15,8 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'jspdx_theme' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'jspdx_theme' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'jspdx_theme' ), 'jspdx_theme', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
+			<?php do_action( 'srbt_demo_theme_credits' ); ?>
+			<span>Copyright &copy; <?php auto_copyright('2012'); ?> <a href="<?php bloginfo('url'); ?>" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a></span>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
--- template-sidebar.php
+++ template-sidebar.php
@@ -1,20 +1,16 @@
 <?php
 /**
- * The template for displaying all pages.
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
+ * Sidebar Template
  *
- * @link https://codex.wordpress.org/Template_Hierarchy
+ * Template Name: Sidebar Page
  *
- * @package JSPDX Theme
+ * @package SR Theme
  */
 
-get_header(); ?>
+get_header('sidebar'); ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="content-area l-main">
 		<main id="main" class="site-main" role="main">
 
 			<?php
@@ -34,4 +30,5 @@ get_header(); ?>
 	</div><!-- #primary -->
 
 <?php
+get_sidebar();
 get_footer();
