f1e9823880361cd8b539e35f8d9b67b4a2e9cf31
Joe Snell <joesnellpdx@gmail.com>
2016-04-14 21:24:02 -0700

    nav-links > .cf

--- comments.php
+++ comments.php
@@ -38,7 +38,7 @@ if ( post_password_required() ) {
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
 		<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
 			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'jspdx_theme' ); ?></h2>
-			<div class="nav-links">
+			<div class="nav-links cf">
 
 				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'jspdx_theme' ) ); ?></div>
 				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'jspdx_theme' ) ); ?></div>
@@ -59,7 +59,7 @@ if ( post_password_required() ) {
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
 		<nav id="comment-nav-below" class="navigation comment-navigation" role="navigation">
 			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'jspdx_theme' ); ?></h2>
-			<div class="nav-links">
+			<div class="nav-links cf">
 
 				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'jspdx_theme' ) ); ?></div>
 				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'jspdx_theme' ) ); ?></div>
