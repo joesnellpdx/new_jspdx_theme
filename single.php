16ba416769b7b285086276d8dcd7e3d18a2b1f46
Joe Snell <joesnellpdx@gmail.com>
2016-04-14 21:27:44 -0700

    nav-links > .cf

--- single.php
+++ single.php
@@ -23,7 +23,7 @@ get_header(); ?>
 
 			<div class="page-extras lc">
 
-			<?php the_post_navigation();
+			<?php //the_post_navigation();
 
 //			// If comments are open or we have at least one comment, load up the comment template.
 //			if ( comments_open() || get_comments_number() ) :
