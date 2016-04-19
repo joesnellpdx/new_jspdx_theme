13d12d501933d427cedaba992c00f6d4e75a330c
Joe Snell <joesnellpdx@gmail.com>
2016-04-18 17:33:56 -0700

    add critical.css

--- inc/theme-functions.php
+++ inc/theme-functions.php
@@ -108,8 +108,16 @@ if ( ! function_exists( 'jspdx_theme_setup' ) ) :
 		 * Add custom fonts
 		 */
 		function add_custom_fonts(){
-		echo '<script src="https://use.typekit.net/xfz2xyu.js"></script>
-<script>try{Typekit.load({ async: true });}catch(e){}</script>';
+		echo "<script>
+  (function(d) {
+    var config = {
+      kitId: 'xfz2xyu',
+      scriptTimeout: 3000,
+      async: true
+    },
+    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,\"\")+\" wf-inactive\";},config.scriptTimeout),tk=d.createElement(\"script\"),f=false,s=d.getElementsByTagName(\"script\")[0],a;h.className+=\" wf-loading\";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!=\"complete\"&&a!=\"loaded\")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+  })(document);
+</script>";
 //	echo '<link rel="stylesheet" href="">';
 		}
 		add_action('wp_head', 'add_custom_fonts');
